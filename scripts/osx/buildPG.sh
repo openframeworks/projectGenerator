cfa6a66201c535b90643f37d6efb2eb841490424
arturo castro <arturo@openframeworks.cc>
2015-07-10 23:21:39 +0200

    build script: merge to master

--- scripts/osx/buildPG.sh
+++ scripts/osx/buildPG.sh
@@ -11,7 +11,7 @@ if [ $ret -ne 0 ]; then
       echo "Failed building Project Generator"
       exit 1
 fi
-if [ "${TRAVIS_REPO_SLUG}/${TRAVIS_BRANCH}" = "openframeworks/projectGenerator/travis" ]; then
+if [ "${TRAVIS_REPO_SLUG}/${TRAVIS_BRANCH}" = "openframeworks/projectGenerator/master" ]; then
     mv projectGeneratorSimple/bin/data/settings/projectGeneratorSettings_production.xml projectGeneratorSimple/bin/data/settings/projectGeneratorSettings.xml
     cp scripts/ssh_config ~/.ssh/config
     chmod 600 scripts/id_rsa
