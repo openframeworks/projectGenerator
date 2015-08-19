27758610a8f50fc7ad2f4eebc8445cda24c352b3
Léo Colombaro <git@colombaro.fr>
2015-08-19 13:29:29 +0200

    Conform to editor configs

--- app.js
+++ app.js
@@ -6,53 +6,53 @@ var ipc = require('ipc');
 var path = require('path');
 
 
-var platforms = ["osx","win_cb", "vs", "ios", "linux", "linux64", "linuxarmv6l", "linuxarmv7l"];
+var platforms = ["osx", "win_cb", "vs", "ios", "linux", "linux64", "linuxarmv6l", "linuxarmv7l"];
 
 var defaultSettings;
 
 
-ipc.on('setOfPath', function(arg) {
+ipc.on('setOfPath', function (arg) {
 	setOFPath(arg);
 });
 
-ipc.on('setup', function(arg) {
+ipc.on('setup', function (arg) {
 	setup();
 });
 
 
 
 //-----------------------------------------
-// this is called from main when defualts are loaded in: 
-ipc.on('setDefaults', function(arg) {
+// this is called from main when defualts are loaded in:
+ipc.on('setDefaults', function (arg) {
 	defaultSettings = arg;
 	setOFPath(defaultSettings['defaultOfPath']);
 });
 
-ipc.on('setProjectPath', function(arg) {
+ipc.on('setProjectPath', function (arg) {
 	var elem = document.getElementById("projectPath");
-	elem.value = arg;	
+	elem.value = arg;
 });
 
-ipc.on('setUpdatePath', function(arg) {
+ipc.on('setUpdatePath', function (arg) {
 	var elem = document.getElementById("updatePath");
-	elem.value = arg;	
+	elem.value = arg;
 });
 
-ipc.on('setAddons', function(arg) {
-	
+ipc.on('setAddons', function (arg) {
+
 	var select = document.getElementById("addonsSelect");
 	select.innerHTML = "";
 
-	if (arg.length > 0){
-		// add: 
-		for (i = 0; i < arg.length; i++){
+	if (arg.length > 0) {
+		// add:
+		for (i = 0; i < arg.length; i++) {
 			var option = document.createElement("option");
 			option.text = arg[i];
 			select.add(option);
 		}
-		$("#addonsSelect").attr( "data-placeholder", "addons..." );
+		$("#addonsSelect").attr("data-placeholder", "addons...");
 	} else {
-		$("#addonsSelect").attr( "data-placeholder", "no addons found, is OF path right?" );
+		$("#addonsSelect").attr("data-placeholder", "no addons found, is OF path right?");
 	}
 
 	$("#addonsSelect").trigger("chosen:updated");
@@ -60,22 +60,22 @@ ipc.on('setAddons', function(arg) {
 
 
 //----------------------------------------
-function setOFPath(arg){
+function setOFPath(arg) {
 
-	// get the element: 
+	// get the element:
 	var elem = document.getElementById("ofPath");
 
-	if( ! path.isAbsolute(arg)) {
+	if (!path.isAbsolute(arg)) {
 
 		// if we are relative, don't do anything...
 
-    	elem.value = arg;					
+		elem.value = arg;
 
-    } else {
+	} else {
 
-		// else check settings for how we want this path.... make relative if we need to: 
+		// else check settings for how we want this path.... make relative if we need to:
 
-		if (defaultSettings['useRelativePath'] == true){
+		if (defaultSettings['useRelativePath'] == true) {
 			var relativePath = path.normalize(path.relative(path.resolve(__dirname), arg)) + "/";
 			elem.value = relativePath;
 		} else {
@@ -83,79 +83,79 @@ function setOFPath(arg){
 		}
 	}
 
-	ipc.send('refreshAddonList','');
-	
-	
+	ipc.send('refreshAddonList', '');
+
+
 }
 
 
 //----------------------------------------
-function setup(){
+function setup() {
 
 
 	$('select').chosen();
 
 	var select = document.getElementById("platformSelect");
 	var selectUpdate = document.getElementById("platformsSelectUpdate");
-	
+
 	select.innerHTML = "";
 	selectUpdate.innerHTML = "";
 
-	for (i = 0; i < platforms.length; i++){
+	for (i = 0; i < platforms.length; i++) {
 		var option = document.createElement("option");
 		option.text = platforms[i];
 		select.add(option);
 	}
 
-	for (i = 0; i < platforms.length; i++){
+	for (i = 0; i < platforms.length; i++) {
 		var option = document.createElement("option");
 		option.text = platforms[i];
 		selectUpdate.add(option);
 	}
 
-	
+
 	$("#platformSelect").trigger("chosen:updated");
 	$("#platformsSelectUpdate").trigger("chosen:updated");
 
-	$( "#projectName" ).val('myApp');
+	$("#projectName").val('myApp');
 }
 
-function generate(){	
+function generate() {
 
-	// let's get all the info: 
+	// let's get all the info:
 
 	var generate = {}
 
-	generate['projectName'] = $( "#projectName" ).val();
-	generate['projectPath']  = $( "#projectPath" ).val();
-	generate['platformList']  = $( "#platformSelect" ).val();
-	generate['addonList']  =  $( "#addonsSelect" ).val();
-	generate['ofPath']  = $( "#ofPath" ).val();
+	generate['projectName'] = $("#projectName").val();
+	generate['projectPath'] = $("#projectPath").val();
+	generate['platformList'] = $("#platformSelect").val();
+	generate['addonList'] = $("#addonsSelect").val();
+	generate['ofPath'] = $("#ofPath").val();
 
 	//console.log(generate);
-		
+
 	ipc.send('generate', generate);
 
 }
 
 
-function update(){	
+function update() {
 
-	// let's get all the info: 
+	// let's get all the info:
 
 	var update = {}
 
 
 
 
-	update['updatePath'] = $( "#updatePath" ).val();
-	update['platformList'] = $( "#platformsSelectUpdate" ).val();
-	update['updateRecursive'] = $( "#platformRecursive" ).is(":checked")
-	update['ofPath']  = $( "#ofPath" ).val();
-//console.log(update);
-	
+	update['updatePath'] = $("#updatePath").val();
+	update['platformList'] = $("#platformsSelectUpdate").val();
+	update['updateRecursive'] = $("#platformRecursive").is(":checked")
+	update['ofPath'] = $("#ofPath").val();
+	//console.log(update);
+
 	// //console.log(generate);
-		
+
 	ipc.send('update', update);
 
 }
@@ -170,6 +170,6 @@ function getProjectPath() {
 	ipc.send('pickProjectPath', '');	// current path could go here
 };
 
-function getUpdatePath(){
+function getUpdatePath() {
 	ipc.send('pickUpdatePath', '');	// current path could go here
 }
