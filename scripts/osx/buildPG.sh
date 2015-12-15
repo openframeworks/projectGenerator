c4525a4e73a8cd17a45cdc595757cf153dd9fe05
Daniel Rosser <danoli3@gmail.com>
2015-12-16 03:34:04 +1100

    Travis Fix.

--- scripts/osx/buildPG.sh
+++ scripts/osx/buildPG.sh
@@ -11,12 +11,13 @@ if [ $ret -ne 0 ]; then
       echo "Failed building Project Generator"
       exit 1
 fi
-if [ "${TRAVIS_REPO_SLUG}/${TRAVIS_BRANCH}" = "openframeworks/projectGenerator/master" ]; then
+echo "${TRAVIS_REPO_SLUG}/${TRAVIS_BRANCH}";
+if [[ "${TRAVIS_REPO_SLUG}/${TRAVIS_BRANCH}" = "openframeworks/projectGenerator/master" && "${TRAVIS_PULL_REQUEST}" = "false" ]]; then
     openssl aes-256-cbc -K $encrypted_cd38768cbb9d_key -iv $encrypted_cd38768cbb9d_iv -in scripts/id_rsa.enc -out scripts/id_rsa -d
     cp scripts/ssh_config ~/.ssh/config
     chmod 600 scripts/id_rsa
     scp -i scripts/id_rsa commandLine/bin/projectGenerator tests@198.61.170.130:projectGenerator_builds/projectGenerator_osx_new
     ssh -i scripts/id_rsa tests@198.61.170.130 "mv projectGenerator_builds/projectGenerator_osx_new projectGenerator_builds/projectGenerator_osx"
 fi
-rm scripts/id_rsa
+rm -rf scripts/id_rsa
 
