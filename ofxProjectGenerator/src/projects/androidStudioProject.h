8cc5896c8e7b52df786e049a1610392c46003a86
arturo castro <arturo@openframeworks.cc>
2016-10-01 17:04:46 +0200

    fix android project not returning

--- ofxProjectGenerator/src/projects/androidStudioProject.h
+++ ofxProjectGenerator/src/projects/androidStudioProject.h
@@ -10,8 +10,8 @@ public:
     void addInclude(std::string includeName){}
     void addLibrary(const LibraryBinary & lib){}
     void addSrc(std::string srcFile, std::string folder, SrcType type=DEFAULT){};
-    bool loadProjectFile(){};
-    bool saveProjectFile(){};
+	bool loadProjectFile() { return false; };
+    bool saveProjectFile(){ return false; };
     static std::string LOG_NAME;
 
 };
