371da44d49d115d2fad7327327afcb2e3cfcb67f
arturo castro <arturo@openframeworks.cc>
2016-10-19 15:18:26 +0200

    buildPG: download libs before building

--- scripts/ios/buildPG.sh
+++ scripts/ios/buildPG.sh
@@ -3,7 +3,11 @@ set -e
 cd ..
 git clone --depth=1 https://github.com/openframeworks/openFrameworks
 mv projectGenerator openFrameworks/apps/
-cd openFrameworks/apps/projectGenerator
+
+cd openFrameworks
+scripts/osx/download_libs.sh
+
+cd apps/projectGenerator
 echo "Building openFrameworks PG - iOS"
 xcodebuild -configuration Release -target projectGeneratorSimple -project projectGeneratorSimple/projectGeneratorSimple.xcodeproj GCC_PREPROCESSOR_DEFINITIONS="MAKE_IOS=1"
 ret=$?
