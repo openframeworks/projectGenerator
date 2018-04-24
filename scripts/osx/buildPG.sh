af234fb6ae74739dc051c49fad37eeffbe889029
arturo castro <arturo@openframeworks.cc>
2018-04-24 15:45:39 +0200

    buildPG: re-enable full build instead of pulling the binary from the ci server

--- scripts/osx/buildPG.sh
+++ scripts/osx/buildPG.sh
@@ -25,23 +25,21 @@ cd ..
 of_root=${PWD}/openFrameworks
 pg_root=${PWD}/openFrameworks/apps/projectGenerator
 
-#brew install npm
-
 git clone --depth=1 https://github.com/openframeworks/openFrameworks
 mv projectGenerator openFrameworks/apps/
 
-# cd ${of_root}
-# scripts/osx/download_libs.sh
+cd ${of_root}
+scripts/osx/download_libs.sh
 
-# Compile commandline tool
-# cd ${pg_root}
-# echo "Building openFrameworks PG - OSX"
-# xcodebuild -configuration Release -target commandLine -project commandLine/commandLine.xcodeproj
-# ret=$?
-# if [ $ret -ne 0 ]; then
-#       echo "Failed building Project Generator"
-#       exit 1
-# fi
+Compile commandline tool
+cd ${pg_root}
+echo "Building openFrameworks PG - OSX"
+xcodebuild -configuration Release -target commandLine -project commandLine/commandLine.xcodeproj
+ret=$?
+if [ $ret -ne 0 ]; then
+      echo "Failed building Project Generator"
+      exit 1
+fi
 
 
 # Generate electron app
@@ -52,8 +50,8 @@ mv dist/projectGenerator-darwin-x64 ${pg_root}/projectGenerator-osx
 
 # Copy commandLine into electron .app
 cd ${pg_root}
-# cp commandLine/bin/projectGenerator projectGenerator-osx/projectGenerator.app/Contents/Resources/app/app/projectGenerator 2> /dev/null
-wget http://ci.openframeworks.cc/projectGenerator/projectGenerator_osx -O projectGenerator-osx/projectGenerator.app/Contents/Resources/app/app/projectGenerator 2> /dev/null
+cp commandLine/bin/projectGenerator projectGenerator-osx/projectGenerator.app/Contents/Resources/app/app/projectGenerator 2> /dev/null
+# wget http://ci.openframeworks.cc/projectGenerator/projectGenerator_osx -O projectGenerator-osx/projectGenerator.app/Contents/Resources/app/app/projectGenerator 2> /dev/null
 sed -i -e "s/osx/osx/g" projectGenerator-osx/projectGenerator.app/Contents/Resources/app/settings.json
 
 # Sign app
@@ -76,28 +74,20 @@ echo "Signing electron .app"
 cd ${pg_root}
 sudo npm install -g electron-osx-sign
 electron-osx-sign projectGenerator-osx/projectGenerator.app
-# appsignPID=$!
-# echoDots $appsignPID
-# wait $appsignPID
-
-# echo "Signing command line binary"
-# codesign --deep --force --verbose --sign "Developer ID Application: Arturo Castro" projectGenerator-osx/projectGenerator.app/Contents/Resources/app/app/projectGenerator
-# clsignPID=$!
-# echoDots $clsignPID
-# wait $clsignPID
 
 echo "Compressing PG app"
 zip -r projectGenerator-osx.zip projectGenerator-osx
 
 # Upload to OF CI server
 echo "${TRAVIS_REPO_SLUG}/${TRAVIS_BRANCH}";
-# if [ "${TRAVIS_REPO_SLUG}/${TRAVIS_BRANCH}" = "openframeworks/projectGenerator/master" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+if [ "${TRAVIS_REPO_SLUG}/${TRAVIS_BRANCH}" = "openframeworks/projectGenerator/master" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     echo "Uploading app to CI servers"
     openssl aes-256-cbc -K $encrypted_cd38768cbb9d_key -iv $encrypted_cd38768cbb9d_iv -in scripts/id_rsa.enc -out scripts/id_rsa -d
     cp scripts/ssh_config ~/.ssh/config
     chmod 600 scripts/id_rsa
     scp -i scripts/id_rsa projectGenerator-osx.zip tests@198.61.170.130:projectGenerator_builds/projectGenerator-osx_new.zip
     ssh -i scripts/id_rsa tests@198.61.170.130 "mv projectGenerator_builds/projectGenerator-osx_new.zip projectGenerator_builds/projectGenerator-osx.zip"
-# fi
+fi
 rm -rf scripts/id_rsa
+rm -rf scripts/developer_ID.p12
 
