183613844f4425e44e9229a82639b4b73c03641d
arturo castro <arturo@openframeworks.cc>
2018-04-24 16:03:06 +0200

    build: try to sign for distribution instead of development

--- scripts/osx/buildPG.sh
+++ scripts/osx/buildPG.sh
@@ -28,18 +28,18 @@ pg_root=${PWD}/openFrameworks/apps/projectGenerator
 git clone --depth=1 https://github.com/openframeworks/openFrameworks
 mv projectGenerator openFrameworks/apps/
 
-cd ${of_root}
-scripts/osx/download_libs.sh
+# cd ${of_root}
+# scripts/osx/download_libs.sh
 
-# Compile commandline tool
-cd ${pg_root}
-echo "Building openFrameworks PG - OSX"
-xcodebuild -configuration Release -target commandLine -project commandLine/commandLine.xcodeproj
-ret=$?
-if [ $ret -ne 0 ]; then
-      echo "Failed building Project Generator"
-      exit 1
-fi
+# # Compile commandline tool
+# cd ${pg_root}
+# echo "Building openFrameworks PG - OSX"
+# xcodebuild -configuration Release -target commandLine -project commandLine/commandLine.xcodeproj
+# ret=$?
+# if [ $ret -ne 0 ]; then
+#       echo "Failed building Project Generator"
+#       exit 1
+# fi
 
 
 # Generate electron app
@@ -50,12 +50,12 @@ mv dist/projectGenerator-darwin-x64 ${pg_root}/projectGenerator-osx
 
 # Copy commandLine into electron .app
 cd ${pg_root}
-cp commandLine/bin/projectGenerator projectGenerator-osx/projectGenerator.app/Contents/Resources/app/app/projectGenerator 2> /dev/null
-# wget http://ci.openframeworks.cc/projectGenerator/projectGenerator_osx -O projectGenerator-osx/projectGenerator.app/Contents/Resources/app/app/projectGenerator 2> /dev/null
+# cp commandLine/bin/projectGenerator projectGenerator-osx/projectGenerator.app/Contents/Resources/app/app/projectGenerator 2> /dev/null
+wget http://ci.openframeworks.cc/projectGenerator/projectGenerator_osx -O projectGenerator-osx/projectGenerator.app/Contents/Resources/app/app/projectGenerator 2> /dev/null
 sed -i -e "s/osx/osx/g" projectGenerator-osx/projectGenerator.app/Contents/Resources/app/settings.json
 
 echo "${TRAVIS_REPO_SLUG}/${TRAVIS_BRANCH}";
-if [ "${TRAVIS_REPO_SLUG}/${TRAVIS_BRANCH}" = "openframeworks/projectGenerator/master" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+# if [ "${TRAVIS_REPO_SLUG}/${TRAVIS_BRANCH}" = "openframeworks/projectGenerator/master" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 # Sign app
     echo "Decoding signing certificates"
     cd ${pg_root}/scripts
@@ -75,7 +75,7 @@ if [ "${TRAVIS_REPO_SLUG}/${TRAVIS_BRANCH}" = "openframeworks/projectGenerator/m
     echo "Signing electron .app"
     cd ${pg_root}
     sudo npm install -g electron-osx-sign
-    electron-osx-sign projectGenerator-osx/projectGenerator.app
+    electron-osx-sign projectGenerator-osx/projectGenerator.app --platform=darwin --type=distribution
 
     echo "Compressing PG app"
     zip -r -q projectGenerator-osx.zip projectGenerator-osx
@@ -87,7 +87,7 @@ if [ "${TRAVIS_REPO_SLUG}/${TRAVIS_BRANCH}" = "openframeworks/projectGenerator/m
     chmod 600 scripts/id_rsa
     scp -i scripts/id_rsa projectGenerator-osx.zip tests@198.61.170.130:projectGenerator_builds/projectGenerator-osx_new.zip
     ssh -i scripts/id_rsa tests@198.61.170.130 "mv projectGenerator_builds/projectGenerator-osx_new.zip projectGenerator_builds/projectGenerator-osx.zip"
-fi
+# fi
 rm -rf scripts/id_rsa
 rm -rf scripts/developer_ID.p12
 
