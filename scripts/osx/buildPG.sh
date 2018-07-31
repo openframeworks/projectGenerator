c5d13c9454826499e1db277139d3eebc303cf412
arturo castro <arturo@openframeworks.cc>
2018-07-31 16:03:33 +0200

    fixing codesign

--- scripts/osx/buildPG.sh
+++ scripts/osx/buildPG.sh
@@ -50,7 +50,8 @@ sign_and_upload(){
         sudo npm install -g electron-osx-sign
         xattr -cr projectGenerator-$PLATFORM/projectGenerator.app
         echo codesign electron
-        mv "projectGenerator-$PLATFORM/projectGenerator.app/Contents/Frameworks/Electron\ Framework.framework/Electron\ Framework" "projectGenerator-$PLATFORM/projectGenerator.app/Contents/Frameworks/Electron\ Framework.framework/Electron_Framework"
+        ls -la "projectGenerator-$PLATFORM/projectGenerator.app/Contents/Frameworks/Electron Framework.framework"
+        mv "projectGenerator-$PLATFORM/projectGenerator.app/Contents/Frameworks/Electron Framework.framework/Electron Framework" "projectGenerator-$PLATFORM/projectGenerator.app/Contents/Frameworks/Electron Framework.framework/Electron_Framework"
         codesign --deep --force --verbose --sign "Developer ID Application: Arturo Castro" "projectGenerator-$PLATFORM/projectGenerator.app/Contents/Frameworks/Electron Framework.framework"
         # electron-osx-sign projectGenerator-$PLATFORM/projectGenerator.app --platform=darwin --type=distribution
         echo codesign pg
