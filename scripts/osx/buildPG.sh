77f7cff319b45d8ee1ffc1248c1e8a0fbec042d8
ofTheo <theo@openframeworks.cc>
2021-04-06 18:07:06 -0700

    debugging error

--- scripts/osx/buildPG.sh
+++ scripts/osx/buildPG.sh
@@ -22,6 +22,8 @@ echoDots(){
 
 sign_and_upload(){
     PLATFORM=$1
+	echo " sign_and_upload ";
+
     # Copy commandLine into electron .app
     cd ${pg_root}
     cp commandLine/bin/projectGenerator projectGenerator-$PLATFORM/projectGenerator.app/Contents/Resources/app/app/projectGenerator 2> /dev/null
@@ -154,8 +156,11 @@ cd ${pg_root}/frontend
 npm update
 npm install > /dev/null
 npm run build:osx > /dev/null
+echo " pre move ";
 mv dist/projectGenerator-darwin-x64 ${pg_root}/projectGenerator-osx
+echo " post move ";
 sign_and_upload osx
+echo " post sign and upload ";
 
 cd ${pg_root}/frontend
 npm run build:osx > /dev/null
