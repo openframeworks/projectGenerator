7c9053cce9094b1f3a0bda779813702996589409
Dimitre <dimitre@users.noreply.github.com>
2022-04-13 22:56:43 -0300

    minor fixes

--- ofxProjectGenerator/src/projects/xcodeProject.cpp
+++ ofxProjectGenerator/src/projects/xcodeProject.cpp
@@ -3,42 +3,42 @@
 
 xcodeProject::xcodeProject(std::string target)
 :baseProject(target){
-    alert("xcodeProject");
+	alert("xcodeProject");
 
 	// FIXME: remove unused variables
-    if( target == "osx" ){
+	if( target == "osx" ){
 		// FIXME: get this UUIDs for IOS too
 		buildConfigurationListUUID = "E4B69B5A0A3A1756003C02F2";
 		buildActionMaskUUID = "E4B69B580A3A1756003C02F2";
 
-        projRootUUID    = "E4B69B4A0A3A1720003C02F2";
+		projRootUUID    = "E4B69B4A0A3A1720003C02F2";
 //        srcUUID         = "E4B69E1C0A3A1BDC003C02F2"; // covered in map
 //        addonUUID       = "BB4B014C10F69532006C3DED"; // covered in map
 //        localAddonUUID  = "6948EE371B920CB800B5AC1A"; // covered in map
 //        buildPhaseUUID  = "E4B69E200A3A1BDC003C02F2";   // not needed anymore, we use buildactionmask UUID 
-        resourcesUUID   = "";
-        frameworksUUID  = "E7E077E715D3B6510020DFD4";   //PBXFrameworksBuildPhase
-        afterPhaseUUID  = "928F60851B6710B200E2D791";
-        buildPhasesUUID  = "E4C2427710CC5ABF004149E2";
-        frameworksBuildPhaseUUID = "E4328149138ABC9F0047C5CB";
-        
-    }else{
-        projRootUUID    = "29B97314FDCFA39411CA2CEA";
+		resourcesUUID   = "";
+		frameworksUUID  = "E7E077E715D3B6510020DFD4";   //PBXFrameworksBuildPhase
+		afterPhaseUUID  = "928F60851B6710B200E2D791";
+		buildPhasesUUID  = "E4C2427710CC5ABF004149E2";
+		frameworksBuildPhaseUUID = "E4328149138ABC9F0047C5CB";
+		
+	}else{
+		projRootUUID    = "29B97314FDCFA39411CA2CEA";
 //        srcUUID         = "E4D8936A11527B74007E1F53";
 //        addonUUID       = "BB16F26B0F2B646B00518274";
 //        localAddonUUID  = "6948EE371B920CB800B5AC1A";
 //        buildPhaseUUID  = "E4D8936E11527B74007E1F53";
-        resourcesUUID   = "BB24DD8F10DA77E000E9C588";
-        buildPhaseResourcesUUID = "BB24DDCA10DA781C00E9C588";
-        frameworksUUID  = "1DF5F4E00D08C38300B7A737";   //PBXFrameworksBuildPhase  // todo: check this?
-        afterPhaseUUID  = "928F60851B6710B200E2D791";
-        buildPhasesUUID = "9255DD331112741900D6945E";   //
+		resourcesUUID   = "BB24DD8F10DA77E000E9C588";
+		buildPhaseResourcesUUID = "BB24DDCA10DA781C00E9C588";
+		frameworksUUID  = "1DF5F4E00D08C38300B7A737";   //PBXFrameworksBuildPhase  // todo: check this?
+		afterPhaseUUID  = "928F60851B6710B200E2D791";
+		buildPhasesUUID = "9255DD331112741900D6945E";   //
 		
 		folderUUID["src"] = srcUUID;
 		folderUUID["addons"] = addonUUID;
 		folderUUID["localAddons"] = localAddonUUID;
 		folderUUID[""] = projRootUUID;
-    }
+	}
 };
 
 
@@ -371,6 +371,8 @@ void xcodeProject::addSrc(std::string srcFile, std::string folder, SrcType type)
 		commands.emplace_back("Add :objects:"+resUUID+":isa string PBXBuildFile");
 		
 		// FIXME: testar se no IOS ta indo tudo bem. este aqui deve ser equivalente ao proximo q esta comentado.
+		
+		// FIXME: XAXA
 		commands.emplace_back("Add :objects:"+resourcesUUID+": string "+resUUID);
 	}
 
@@ -390,80 +392,84 @@ void xcodeProject::addSrc(std::string srcFile, std::string folder, SrcType type)
 
 void xcodeProject::addFramework(std::string name, std::string path, std::string folder){
 	alert ("addFramework name:" + name + " -- path:" + path + " -- folder:" + folder);
-    // name = name of the framework
-    // path = the full path (w name) of this framework
-    // folder = the path in the addon (in case we want to add this to the file browser -- we don't do that for system libs);
-    
-    //-----------------------------------------------------------------
-    // based on the extension make some choices about what to do:
-    //-----------------------------------------------------------------
-    
-    //-----------------------------------------------------------------
-    // (A) make a FILE REF
-    //-----------------------------------------------------------------
-    
-    // encoding may be messing up for frameworks... so I switched to a pbx file ref without encoding fields
-    std::string UUID = generateUUID( name );
+	// name = name of the framework
+	// path = the full path (w name) of this framework
+	// folder = the path in the addon (in case we want to add this to the file browser -- we don't do that for system libs);
+	
+	//-----------------------------------------------------------------
+	// based on the extension make some choices about what to do:
+	//-----------------------------------------------------------------
+	
+	//-----------------------------------------------------------------
+	// (A) make a FILE REF
+	//-----------------------------------------------------------------
+	
+	// encoding may be messing up for frameworks... so I switched to a pbx file ref without encoding fields
+	std::string UUID = generateUUID( name );
 
-    commands.emplace_back("Add :objects:"+UUID+":lastKnownFileType string wrapper.framework");
-    commands.emplace_back("Add :objects:"+UUID+":fileEncoding string 4");
-    commands.emplace_back("Add :objects:"+UUID+":isa string PBXFileReference");
-    commands.emplace_back("Add :objects:"+UUID+":name string "+name);
-    commands.emplace_back("Add :objects:"+UUID+":path string "+path);
+	commands.emplace_back("Add :objects:"+UUID+":lastKnownFileType string wrapper.framework");
+//	commands.emplace_back("Add :objects:"+UUID+":fileEncoding string 4");
+	commands.emplace_back("Add :objects:"+UUID+":isa string PBXFileReference");
+	commands.emplace_back("Add :objects:"+UUID+":name string "+name);
+	commands.emplace_back("Add :objects:"+UUID+":path string "+path);
 	commands.emplace_back("Add :objects:"+UUID+":sourceTree string <group>");
 	
 	
-    std::string buildUUID = generateUUID(name + "-build");
+	std::string buildUUID = generateUUID(name + "-build");
 	commands.emplace_back("Add :objects:"+buildUUID+":fileRef string "+UUID);
 	commands.emplace_back("Add :objects:"+buildUUID+":isa string PBXBuildFile");
 	
 
-    // we add one of the build refs to the list of frameworks
-	commands.emplace_back("Add :options:"+frameworksUUID+": string " + buildUUID);
+	// we add one of the build refs to the list of frameworks
+	// FIXME: removi aqui pra testar
+	// E7E077E715D3B6510020DFD4
+	commands.emplace_back("Add :objects:"+frameworksUUID+":children array");
+	commands.emplace_back("Add :objects:"+frameworksUUID+":children: string " + buildUUID);
 
-    // we add the second to a final build phase for copying the framework into app.   we need to make sure we *don't* do this for system frameworks
-    
-    if (folder.size() != 0 && !ofIsStringInString(path, "/System/Library/Frameworks")
-        && target != "ios"){
-        
-        std::string buildUUID2 = generateUUID(name + "-build2");
-		commands.emplace_back("Add :options:"+buildUUID2+":fileRef string "+UUID);
-		commands.emplace_back("Add :options:"+buildUUID2+":isa string PBXBuildFile");
+	// we add the second to a final build phase for copying the framework into app.   we need to make sure we *don't* do this for system frameworks
+	
+	if (folder.size() != 0 && !ofIsStringInString(path, "/System/Library/Frameworks")
+		&& target != "ios"){
+		
+		std::string buildUUID2 = generateUUID(name + "-build2");
+		commands.emplace_back("Add :objects:"+buildUUID2+":fileRef string "+UUID);
+		commands.emplace_back("Add :objects:"+buildUUID2+":isa string PBXBuildFile");
 		
 		// UUID hardcoded para PBXCopyFilesBuildPhase
 		// FIXME: hardcoded
-		commands.emplace_back("Add :options:E4C2427710CC5ABF004149E2:files: string " + buildUUID2);
-    }
-    
-    // now, we get the path for this framework without the name
+		commands.emplace_back("Add :objects:E4C2427710CC5ABF004149E2:files: string " + buildUUID2);
+	}
+	
+	// now, we get the path for this framework without the name
 
-    std::string pathWithoutName;
-    std::vector < std::string > pathSplit = ofSplitString(path, "/");
-    for (int i = 0; i < pathSplit.size()-1; i++){
-        if (i != 0) pathWithoutName += "/";
-        pathWithoutName += pathSplit[i];
-    }
-    
-    // then, we are going to add this to "FRAMEWORK_SEARCH_PATHS" -- we do this twice, once for debug once for release.
+	std::string pathWithoutName;
+	std::vector < std::string > pathSplit = ofSplitString(path, "/");
+	for (int i = 0; i < pathSplit.size()-1; i++){
+		if (i != 0) pathWithoutName += "/";
+		pathWithoutName += pathSplit[i];
+	}
+	
+	// then, we are going to add this to "FRAMEWORK_SEARCH_PATHS" -- we do this twice, once for debug once for release.
 	
 	for (auto & c : buildConfigs) {
-		commands.emplace_back("Add :objects:"+c+":buildSettings:FRAMEWORK_SEARCH_PATHS: string " + pathWithoutName);
+		commands.emplace_back
+		("Add :objects:"+c+":buildSettings:FRAMEWORK_SEARCH_PATHS: string " + pathWithoutName);
 	}
 
-    // finally, this is for making folders based on the frameworks position in the addon. so it can appear in the sidebar / file explorer
-    
-    if (folder.size() > 0 && !ofIsStringInString(folder, "/System/Library/Frameworks")){
+	// finally, this is for making folders based on the frameworks position in the addon. so it can appear in the sidebar / file explorer
+	
+	if (folder.size() > 0 && !ofIsStringInString(folder, "/System/Library/Frameworks")){
 //		std::cout << "this " <<  folder << std::endl;
 		std::string folderUUID = getFolderUUID(folder);
-    } else { //FIXME: else what?
-        
-    }
-    
-    if (target != "ios" && folder.size() != 0){
-        // add it to the linking phases...
+	} else { //FIXME: else what?
+		
+	}
+	
+	if (target != "ios" && folder.size() != 0){
+		// add it to the linking phases...
 		// FIXME: hardcoded UUID
 		commands.emplace_back("Add :objects:E4B69B590A3A1756003C02F2:files: string "+buildUUID);
-    }
+	}
 }
 
 void xcodeProject::addInclude(std::string includeName){
@@ -490,9 +496,12 @@ void xcodeProject::addLDFLAG(std::string ldflag, LibType libType){
 }
 
 void xcodeProject::addCFLAG(std::string cflag, LibType libType){
-	for (auto & c : buildConfigs) {
+	alert("addCFLAG " + cflag);
+
+	for (auto & c : buildConfigurations) {
 		// FIXME: add array here if it doesnt exist
 		// FIXME: Test everything
+		commands.emplace_back("Add :objects:"+c+":buildSettings:OTHER_CFLAGS array");
 		commands.emplace_back("Add :objects:"+c+":buildSettings:OTHER_CFLAGS: string " + cflag);
 	}
 }
@@ -533,14 +542,14 @@ void xcodeProject::addAddon(ofAddon & addon){
 	alert("addAddon : " + addon.name);
 
 	for(int i=0;i<(int)addons.size();i++){
-        if(addons[i].name==addon.name){
-            return;
+		if(addons[i].name==addon.name){
+			return;
 		}
 	}
 
-    for(int i=0;i<addon.dependencies.size();i++){
-        baseProject::addAddon(addon.dependencies[i]);
-    }
+	for(int i=0;i<addon.dependencies.size();i++){
+		baseProject::addAddon(addon.dependencies[i]);
+	}
 
 	for(int i=0;i<addon.dependencies.size();i++){
 		for(int j=0;j<(int)addons.size();j++){
@@ -552,68 +561,68 @@ void xcodeProject::addAddon(ofAddon & addon){
 		}
 	}
 	
-    addons.push_back(addon);
+	addons.push_back(addon);
 
-    for(int i=0;i<(int)addon.includePaths.size();i++){
-        ofLogVerbose() << "adding addon include path: " << addon.includePaths[i];
-        addInclude(addon.includePaths[i]);
-    }
-    for(int i=0;i<(int)addon.libs.size();i++){
-        ofLogVerbose() << "adding addon libs: " << addon.libs[i].path;
-        addLibrary(addon.libs[i]);
-    }
-    for(int i=0;i<(int)addon.cflags.size();i++){
-        ofLogVerbose() << "adding addon cflags: " << addon.cflags[i];
-        addCFLAG(addon.cflags[i]);
-    }
+	for(int i=0;i<(int)addon.includePaths.size();i++){
+		ofLogVerbose() << "adding addon include path: " << addon.includePaths[i];
+		addInclude(addon.includePaths[i]);
+	}
+	for(int i=0;i<(int)addon.libs.size();i++){
+		ofLogVerbose() << "adding addon libs: " << addon.libs[i].path;
+		addLibrary(addon.libs[i]);
+	}
+	for(int i=0;i<(int)addon.cflags.size();i++){
+		ofLogVerbose() << "adding addon cflags: " << addon.cflags[i];
+		addCFLAG(addon.cflags[i]);
+	}
 	for(int i=0;i<(int)addon.cppflags.size();i++){
-        ofLogVerbose() << "adding addon cppflags: " << addon.cppflags[i];
-        addCPPFLAG(addon.cppflags[i]);
-    }
-    for(int i=0;i<(int)addon.ldflags.size();i++){
-        ofLogVerbose() << "adding addon ldflags: " << addon.ldflags[i];
-        addLDFLAG(addon.ldflags[i]);
-    }
-    std::sort(addon.srcFiles.begin(), addon.srcFiles.end(), std::less<std::string>());
-    for(int i=0;i<(int)addon.srcFiles.size(); i++){
-        ofLogVerbose() << "adding addon srcFiles: " << addon.srcFiles[i];
-        addSrc(addon.srcFiles[i],addon.filesToFolders[addon.srcFiles[i]]);
-    }
+		ofLogVerbose() << "adding addon cppflags: " << addon.cppflags[i];
+		addCPPFLAG(addon.cppflags[i]);
+	}
+	for(int i=0;i<(int)addon.ldflags.size();i++){
+		ofLogVerbose() << "adding addon ldflags: " << addon.ldflags[i];
+		addLDFLAG(addon.ldflags[i]);
+	}
+	std::sort(addon.srcFiles.begin(), addon.srcFiles.end(), std::less<std::string>());
+	for(int i=0;i<(int)addon.srcFiles.size(); i++){
+		ofLogVerbose() << "adding addon srcFiles: " << addon.srcFiles[i];
+		addSrc(addon.srcFiles[i],addon.filesToFolders[addon.srcFiles[i]]);
+	}
 	for(int i=0;i<(int)addon.defines.size(); i++){
 		ofLogVerbose() << "adding addon defines: " << addon.defines[i];
 		addDefine(addon.defines[i]);
 	}
 
-    for(int i=0;i<(int)addon.frameworks.size(); i++){
-        ofLogVerbose() << "adding addon frameworks: " << addon.frameworks[i];
-        
-        size_t found=addon.frameworks[i].find('/');
-        if (found==std::string::npos){
-            if (target == "ios"){
-                addFramework( addon.frameworks[i] + ".framework", "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System/Library/Frameworks/" + addon.frameworks[i] + ".framework", "addons/" + addon.name + "/frameworks");
-            } else {
-             addFramework( addon.frameworks[i] + ".framework", "/System/Library/Frameworks/" + addon.frameworks[i] + ".framework", "addons/" + addon.name + "/frameworks");
-            }
-        } else {
-            
-            if (ofIsStringInString(addon.frameworks[i], "/System/Library")){
-                
-                std::vector < std::string > pathSplit = ofSplitString(addon.frameworks[i], "/");
-                
-                addFramework(pathSplit[pathSplit.size()-1],
-                             addon.frameworks[i],
-                             "addons/" + addon.name + "/frameworks");
-                
-            } else {
-            
-                std::vector < std::string > pathSplit = ofSplitString(addon.frameworks[i], "/");
-                
-                addFramework(pathSplit[pathSplit.size()-1],
-                             addon.frameworks[i],
-                             addon.filesToFolders[addon.frameworks[i]]);
-            }
-        }
-    }
+	for(int i=0;i<(int)addon.frameworks.size(); i++){
+		ofLogVerbose() << "adding addon frameworks: " << addon.frameworks[i];
+		
+		size_t found=addon.frameworks[i].find('/');
+		if (found==std::string::npos){
+			if (target == "ios"){
+				addFramework( addon.frameworks[i] + ".framework", "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System/Library/Frameworks/" + addon.frameworks[i] + ".framework", "addons/" + addon.name + "/frameworks");
+			} else {
+			 addFramework( addon.frameworks[i] + ".framework", "/System/Library/Frameworks/" + addon.frameworks[i] + ".framework", "addons/" + addon.name + "/frameworks");
+			}
+		} else {
+			
+			if (ofIsStringInString(addon.frameworks[i], "/System/Library")){
+				
+				std::vector < std::string > pathSplit = ofSplitString(addon.frameworks[i], "/");
+				
+				addFramework(pathSplit[pathSplit.size()-1],
+							 addon.frameworks[i],
+							 "addons/" + addon.name + "/frameworks");
+				
+			} else {
+			
+				std::vector < std::string > pathSplit = ofSplitString(addon.frameworks[i], "/");
+				
+				addFramework(pathSplit[pathSplit.size()-1],
+							 addon.frameworks[i],
+							 addon.filesToFolders[addon.frameworks[i]]);
+			}
+		}
+	}
 }
 
 bool xcodeProject::saveProjectFile(){
@@ -622,7 +631,7 @@ bool xcodeProject::saveProjectFile(){
 	std::string command = "/usr/libexec/PlistBuddy " + fileName;
 	for (auto & c : commands) {
 		command += " -c \"" + c + "\"";
-//		std::cout << c << std::endl;
+		std::cout << c << std::endl;
 	}
 	std::cout << ofSystem(command) << std::endl;
 	
--- ofxProjectGenerator/src/projects/xcodeProject.h
+++ ofxProjectGenerator/src/projects/xcodeProject.h
@@ -9,54 +9,54 @@ class xcodeProject : public baseProject {
 
 public:
 
-    xcodeProject(std::string target);
+	xcodeProject(std::string target);
 
 private:
 
-    bool createProjectFile();
-    bool loadProjectFile();
-    bool saveProjectFile();
-    void saveMakefile();
+	bool createProjectFile();
+	bool loadProjectFile();
+	bool saveProjectFile();
+	void saveMakefile();
 
 public:
 
-    void addSrc(std::string srcFile, std::string folder, SrcType type=DEFAULT);
-    void addInclude(std::string includeName);
-    void addLibrary(const LibraryBinary & lib);
-    void addLDFLAG(std::string ldflag, LibType libType = RELEASE_LIB);
-    void addCFLAG(std::string cflag, LibType libType = RELEASE_LIB); // Other C Flags
-    void addCPPFLAG(std::string cppflag, LibType libType = RELEASE_LIB); // Other C++ Flags
-    void addAfterRule(std::string script);
+	void addSrc(std::string srcFile, std::string folder, SrcType type=DEFAULT);
+	void addInclude(std::string includeName);
+	void addLibrary(const LibraryBinary & lib);
+	void addLDFLAG(std::string ldflag, LibType libType = RELEASE_LIB);
+	void addCFLAG(std::string cflag, LibType libType = RELEASE_LIB); // Other C Flags
+	void addCPPFLAG(std::string cppflag, LibType libType = RELEASE_LIB); // Other C++ Flags
+	void addAfterRule(std::string script);
 	void addDefine(std::string define, LibType libType = RELEASE_LIB);
-    
-    // specific to OSX
-    void addFramework(std::string name, std::string path, std::string folder="");  // folder if for non system frameworks
-    
-        
-    
-    void addAddon(ofAddon & addon);
+	
+	// specific to OSX
+	void addFramework(std::string name, std::string path, std::string folder="");  // folder if for non system frameworks
+	
+		
+	
+	void addAddon(ofAddon & addon);
 
 //    void saveWorkspaceXML();
-    void saveScheme();
-    void renameProject();
+	void saveScheme();
+	void renameProject();
 
-    std::string projRootUUID;
-    std::string srcUUID;
-    std::string addonUUID;
-    std::string localAddonUUID;
-    std::string resourcesUUID;
-    std::string buildPhaseUUID;                 // note this UUID keeps refs all things to build
-    std::string frameworksUUID;
-    std::string buildPhaseResourcesUUID;
-    std::string frameworksBuildPhaseUUID;
-    std::string afterPhaseUUID;
-    std::string buildPhasesUUID;                // note this UUID is in an array of *all* the build steps 
-    
+	std::string projRootUUID;
+	std::string srcUUID;
+	std::string addonUUID;
+	std::string localAddonUUID;
+	std::string resourcesUUID;
+	std::string buildPhaseUUID;                 // note this UUID keeps refs all things to build
+	std::string frameworksUUID;
+	std::string buildPhaseResourcesUUID;
+	std::string frameworksBuildPhaseUUID;
+	std::string afterPhaseUUID;
+	std::string buildPhasesUUID;                // note this UUID is in an array of *all* the build steps 
+	
 	std::string buildConfigurationListUUID;                // note this UUID is in an array of *all* the build steps
 	std::string buildActionMaskUUID;
-    
+	
 //	pugi::xml_node insertPoint;         // where are we inserting items (at the second dict tag,
-                                        // /plist[1]/dict[1]/dict[2])
+										// /plist[1]/dict[1]/dict[2])
 //    bool findArrayForUUID(std::string UUID, pugi::xml_node & nodeMe);
 
 	
@@ -68,9 +68,11 @@ public:
 		"E4B69B600A3A1757003C02F2", //Debug
 		"99FA3DBC1C7456C400CFA0EE", //AppStore
 
-		"99FA3DBB1C7456C400CFA0EE", //AppStore SDKROOT macosx
+		"E4EB6923138AFD0F00A09F29", //AppStore SDKROOT macosx
 		"E4B69B4E0A3A1720003C02F2", //Debug SDKROOT macosx
 		"E4B69B4F0A3A1720003C02F2", //Release SDKROOT macosx
+		
+		
 	};
 	
 	const std::string buildConfigs[3] = {
@@ -78,10 +80,6 @@ public:
 		"E4B69B600A3A1757003C02F2", //Debug
 		"99FA3DBC1C7456C400CFA0EE", //AppStore
 	};
-	
-	void alert(std::string s) {
-//		std::cout << ">>>>> " << s << std::endl;
-	}
 	
 	std::map <std::string, std::string> folderUUID = {
 		{ "src", "E4B69E1C0A3A1BDC003C02F2" },
@@ -91,4 +89,8 @@ public:
 	};
 	
 	std::string getFolderUUID(std::string folder);
+	
+	void alert(std::string s) {
+		std::cout << ">>>>> " << s << std::endl;
+	}
 };
