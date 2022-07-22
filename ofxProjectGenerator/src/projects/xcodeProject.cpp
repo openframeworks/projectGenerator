82d914afc6fe2a54f8d7369dcfe2a78157f8efad
Dimitre <dimitre@users.noreply.github.com>
2022-07-22 15:19:15 -0300

    baddfolder removal

--- ofxProjectGenerator/src/projects/xcodeProject.cpp
+++ ofxProjectGenerator/src/projects/xcodeProject.cpp
@@ -257,7 +257,7 @@ void xcodeProject::addSrc(std::string srcFile, std::string folder, SrcType type)
 	bool addToBuild = true;
 	bool addToBuildResource = false;
 	std::string fileKind = "file";
-	bool bAddFolder = true;
+//	bool bAddFolder = true;
 
 	if(type==DEFAULT){
 		if( ext == "cpp" || ext == "cc" || ext =="cxx" ){
@@ -331,9 +331,9 @@ void xcodeProject::addSrc(std::string srcFile, std::string folder, SrcType type)
 		}
 	}
 	
-	if (folder == "src"){
-		bAddFolder = false;
-	}
+//	if (folder == "src"){
+//		bAddFolder = false;
+//	}
 
 	//-----------------------------------------------------------------
 	// (A) make a FILE REF
