23a6eb06eb9e6e4605da34c545416d08233000cd
Dimitre <dimitre@users.noreply.github.com>
2023-03-07 20:16:28 -0300

    npm

--- scripts/osx/buildPG.sh
+++ scripts/osx/buildPG.sh
@@ -152,7 +152,7 @@ if [ $ret -ne 0 ]; then
 fi
 
 # install electron sign globally
-# sudo chown -R 501:20 "/Users/runner/.npm"
+sudo chown -R 501:20 "/Users/runner/.npm"
 sudo npm install -g npm@9.6.0
 sudo npm install -g electron/osx-sign
 import_certificate
