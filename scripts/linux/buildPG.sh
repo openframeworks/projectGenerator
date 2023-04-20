4114c18d39dfb653e49f661c965941d1c682e533
Theodore Watson <theo@openframeworks.cc>
2023-04-20 08:30:28 -0700

    comment out uploading as we don't need it

--- scripts/linux/buildPG.sh
+++ scripts/linux/buildPG.sh
@@ -30,24 +30,24 @@ if [[ $errorcode -ne 0 ]]; then
         exit $errorcode
 fi
 
-if [[ -z "${GA_CI_SECRET}" ]] ; then
-    echo "Not on main repo skipping sign and upload";
-else
-    if [[ "${TRAVIS_REPO_SLUG}/${TRAVIS_BRANCH}" == "openframeworks/projectGenerator/master" && "$TRAVIS_PULL_REQUEST" == "false" ]] || [[ "${GITHUB_REF##*/}" == "master" &&  -z "${GITHUB_HEAD_REF}" ]] ; then
+# if [[ -z "${GA_CI_SECRET}" ]] ; then
+#     echo "Not on main repo skipping sign and upload";
+# else
+#     if [[ "${TRAVIS_REPO_SLUG}/${TRAVIS_BRANCH}" == "openframeworks/projectGenerator/master" && "$TRAVIS_PULL_REQUEST" == "false" ]] || [[ "${GITHUB_REF##*/}" == "master" &&  -z "${GITHUB_HEAD_REF}" ]] ; then
 
-        if [ "$GITHUB_ACTIONS" = true ]; then
-            echo Unencrypting key for github actions
-            openssl aes-256-cbc -salt -md md5 -a -d -in scripts/githubactions-id_rsa.enc -out scripts/id_rsa -pass env:GA_CI_SECRET
-            mkdir -p ~/.ssh
-        else
-            echo Unencrypting key for travis
-            openssl aes-256-cbc -K $encrypted_cd38768cbb9d_key -iv $encrypted_cd38768cbb9d_iv -in scripts/id_rsa.enc -out scripts/id_rsa -d
-        fi
+#         if [ "$GITHUB_ACTIONS" = true ]; then
+#             echo Unencrypting key for github actions
+#             openssl aes-256-cbc -salt -md md5 -a -d -in scripts/githubactions-id_rsa.enc -out scripts/id_rsa -pass env:GA_CI_SECRET
+#             mkdir -p ~/.ssh
+#         else
+#             echo Unencrypting key for travis
+#             openssl aes-256-cbc -K $encrypted_cd38768cbb9d_key -iv $encrypted_cd38768cbb9d_iv -in scripts/id_rsa.enc -out scripts/id_rsa -d
+#         fi
 
-        cp scripts/ssh_config ~/.ssh/config
-        chmod 600 scripts/id_rsa
-        scp -i scripts/id_rsa commandLine/bin/projectGenerator tests@198.61.170.130:projectGenerator_builds/projectGenerator_linux_new
-        ssh -i scripts/id_rsa tests@198.61.170.130 "mv projectGenerator_builds/projectGenerator_linux_new projectGenerator_builds/projectGenerator_linux"
-    fi
-    rm -rf scripts/id_rsa
-fi
+#         cp scripts/ssh_config ~/.ssh/config
+#         chmod 600 scripts/id_rsa
+#         scp -i scripts/id_rsa commandLine/bin/projectGenerator tests@198.61.170.130:projectGenerator_builds/projectGenerator_linux_new
+#         ssh -i scripts/id_rsa tests@198.61.170.130 "mv projectGenerator_builds/projectGenerator_linux_new projectGenerator_builds/projectGenerator_linux"
+#     fi
+#     rm -rf scripts/id_rsa
+# fi
