3aa554584ac192845662381cc4e8aca3090b86b0
Dimitre <dimitre@users.noreply.github.com>
2023-07-04 20:44:55 -0300

    update

--- commandLine/src/projects/baseProject.cpp
+++ commandLine/src/projects/baseProject.cpp
@@ -251,7 +251,7 @@ bool baseProject::isAddonInCache(const string & addonPath, const string platform
 }
 
 void baseProject::addAddon(string addonName){
-//	std::cout << "baseProject::addAddon " << addonName << std::endl;
+	std::cout << "baseProject::addAddon " << addonName << std::endl;
 	
 	// FIXME : not target, yes platform.
 	#ifdef TARGET_WIN32
