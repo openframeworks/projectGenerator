ebe387ade7851f422e825a576e15fa9dad5b757e
Dimitre <dimitre@users.noreply.github.com>
2023-07-08 15:35:40 -0300

    std::string

--- commandLine/src/projects/baseProject.cpp
+++ commandLine/src/projects/baseProject.cpp
@@ -14,6 +14,7 @@
 #include <list>
 #include <set>
 #include <unordered_set>
+using std::string;
 
 const fs::path templatesFolder = "scripts/templates";
 
--- commandLine/src/projects/baseProject.h
+++ commandLine/src/projects/baseProject.h
@@ -6,7 +6,6 @@
 
 #include <map>
 namespace fs = of::filesystem;
-using std::string;
 
 class baseProject {
 public:
@@ -25,20 +24,20 @@ public:
 
 	struct Template{
 		ofDirectory dir;
-		string name;
+		std::string name;
 		std::vector<string> platforms;
-		string description;
+		std::string description;
 		std::map<fs::path, fs::path> renames;
 		bool operator<(const Template & other) const{
 			return dir<other.dir;
 		}
 	};
 
-	baseProject(string _target);
+	baseProject(std::string _target);
 
 	virtual ~baseProject(){}
 
-	bool create(const fs::path & path, string templateName="");
+	bool create(const fs::path & path, std::string templateName="");
 	void parseAddons();
 	void parseConfigMake();
 	bool save();
@@ -56,22 +55,22 @@ private:
 public:
 
 	virtual void addSrc(const fs::path & srcFile, const fs::path & folder, SrcType type=DEFAULT) = 0;
-	virtual void addInclude(string includeName) = 0;
+	virtual void addInclude(std::string includeName) = 0;
 	virtual void addLibrary(const LibraryBinary & lib) = 0;
-	virtual void addLDFLAG(string ldflag, LibType libType = RELEASE_LIB){}
-	virtual void addCFLAG(string cflag, LibType libType = RELEASE_LIB){} // C_FLAGS
-	virtual void addCPPFLAG(string cppflag, LibType libType = RELEASE_LIB){} // CXX_FLAGS
-	virtual void addAfterRule(string script){}
-	virtual void addDefine(string define, LibType libType = RELEASE_LIB) {}
+	virtual void addLDFLAG(std::string ldflag, LibType libType = RELEASE_LIB){}
+	virtual void addCFLAG(std::string cflag, LibType libType = RELEASE_LIB){} // C_FLAGS
+	virtual void addCPPFLAG(std::string cppflag, LibType libType = RELEASE_LIB){} // CXX_FLAGS
+	virtual void addAfterRule(std::string script){}
+	virtual void addDefine(std::string define, LibType libType = RELEASE_LIB) {}
 
-	virtual void addAddon(string addon);
+	virtual void addAddon(std::string addon);
 	virtual void addAddon(ofAddon & addon);
 	virtual void addSrcRecursively(const fs::path & srcPath);
 
-	string getName() { return projectName;}
+	std::string getName() { return projectName;}
 	fs::path getPath() { return projectDir; }
 
-	std::vector<Template> listAvailableTemplates(string target);
+	std::vector<Template> listAvailableTemplates(std::string target);
 	std::unique_ptr<baseProject::Template> parseTemplate(const fs::path & templateDir);
 	virtual fs::path getPlatformTemplateDir();
 
@@ -80,8 +79,8 @@ public:
 
 	fs::path projectDir;
 	fs::path templatePath;
-	string projectName;
-	string target;
+	std::string projectName;
+	std::string target;
 	
 	bool bMakeRelative = false;
 
@@ -91,9 +90,9 @@ protected:
 	bool recursiveCopy(const fs::path & srcDir, const fs::path & destDir);
 
 	std::vector<ofAddon> addons;
-	std::vector<string> extSrcPaths;
+	std::vector<std::string> extSrcPaths;
 
 	//cached addons - if an addon is requested more than once, avoid loading from disk as it's quite slow
-	std::unordered_map<string,std::unordered_map<string, ofAddon>> addonsCache; //indexed by [platform][supplied path]
-	bool isAddonInCache(const string & addonPath, const string platform); //is this addon in the mem cache?
+	std::unordered_map<std::string,std::unordered_map<std::string, ofAddon>> addonsCache; //indexed by [platform][supplied path]
+	bool isAddonInCache(const std::string & addonPath, const std::string platform); //is this addon in the mem cache?
 };
