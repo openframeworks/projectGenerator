1faddda93f444727c52a366c371a95bfd08c8deb
Dimitre <dimitre@users.noreply.github.com>
2023-07-28 23:21:17 -0300

    fix path for src/eye folder

--- commandLine/src/projects/baseProject.cpp
+++ commandLine/src/projects/baseProject.cpp
@@ -205,28 +205,26 @@ bool baseProject::create(const fs::path & path, string templateName){
 		}
 
 		// only add unique paths
-		vector < fs::path > paths;
-		for (auto & f : fileNames) {
-			auto dir = fs::path(f).parent_path().filename();
-			if (std::find(paths.begin(), paths.end(), dir) == paths.end()) {
-				paths.emplace_back(dir);
-				addInclude(dir.string());
-			}
-		}
-		
 //		vector < fs::path > paths;
 //		for (auto & f : fileNames) {
-////			auto dir = fs::path(f).parent_path().filename();
-//			fs::path rel { fs::relative(fs::path(f).parent_path(), projectDir) };
-//			if (std::find(paths.begin(), paths.end(), rel) == paths.end()) {
-//				paths.emplace_back(rel);
-////				alert ("f = " + f);
-////				alert ("dir = " + dir.string());
-////				alert ("rel = " + rel.string());
-//
-//				addInclude(rel.string());
+//			auto dir = fs::path(f).parent_path().filename();
+//			if (std::find(paths.begin(), paths.end(), dir) == paths.end()) {
+//				paths.emplace_back(dir);
+//				addInclude(dir.string());
 //			}
 //		}
+
+		// FIXME: Port to std::list, so no comparison is needed.
+		// only add unique paths
+		vector < fs::path > paths;
+		for (auto & f : fileNames) {
+			fs::path rel { fs::relative(fs::path(f).parent_path(), projectDir) };
+			if (std::find(paths.begin(), paths.end(), rel) == paths.end()) {
+				paths.emplace_back(rel);
+//				alert ("rel = " + rel.string());
+				addInclude(rel.string());
+			}
+		}
 	}
 	return true;
 }
