d67fdb77ad38373b0a36bc5775d928ec201c5b28
Dan Rosser <danoli3@gmail.com>
2023-12-15 21:46:32 +1100

    vs front end copy
    (cherry picked from commit a8c3e75fa12d0dfcb12ab18a4a44ef9fa14519ea)

--- scripts/vs/build_frontend.sh
+++ scripts/vs/build_frontend.sh
@@ -18,21 +18,41 @@ if [ -z "${BUILD_TEST+x}" ]; then
    BUILD_TEST=0
 fi
 
+
 echo "====== "
 cd "${PG_DIR}/frontend"
 pwd 
+
+# Assuming CMDLINE_DIR is already set to the correct directory path
+SOURCE_FILE="${PG_DIR}/commandLine/bin/commandLine.exe"
+
+# Replace [destination_path] with the actual path where you want to copy the file
+DESTINATION_PATH="app/"
+echo "SOURCE_FILE:$SOURCE_FILE";
+# Check if the source file exists
+if [ -f "$SOURCE_FILE" ]; then
+    # File exists, proceed with copying
+    mkdir -p "$DESTINATION_PATH" # Create destination directory if it doesn't exist
+    cp "$SOURCE_FILE" "$DESTINATION_PATH/projectGenerator.exe"
+    echo "projectGenerator.exe File copied successfully."
+else
+    # File does not exist
+    echo " projectGenerator.exe Error: Source file does not exist."
+fi
+
+
 echo "Building openFrameworks Frontend ${FRONTEND_TARGET}"
 npm install
 echo "====== install"
-npm update > /dev/null
+npm update 
 echo "====== update"
-npm run > /dev/null
+npm run 
 echo "====== run"
 
-if [ "${BUILD_TEST}" == 1 ]; then
+# if [ "${BUILD_TEST}" == 1 ]; then
    npm run start:prod
    echo "====== start:prod"
-fi
+# fi
 
 npm run dist:${FRONTEND_TARGET}
 echo "====== dist:${FRONTEND_TARGET}"
