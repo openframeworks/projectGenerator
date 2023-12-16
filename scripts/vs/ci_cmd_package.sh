14d407dbfaf17db95cef5529090146c149e27321
Dan Rosser <danoli3@gmail.com>
2023-12-17 04:10:58 +1100

    vs workflow

--- scripts/vs/ci_cmd_package.sh
+++ scripts/vs/ci_cmd_package.sh
@@ -25,3 +25,21 @@ else
     echo "commandLine.exe does not exist. please build first"
 fi
 
+cd ${PG_DIR}
+
+SOURCE_FILE="${PG_DIR}/commandLine/bin/projectGenerator.exe"
+FRONTEND_DIR="$( cd "$( dirname "${PG_DIR}/frontend" )" && pwd )"
+cd "${PG_DIR}/frontend"
+
+DESTINATION_PATH="app/"
+echo "SOURCE_FILE:$SOURCE_FILE";
+# Check if the source file exists
+if [ -f "$SOURCE_FILE" ]; then
+    # File exists, proceed with copying
+    mkdir -p "$DESTINATION_PATH" # Create destination directory if it doesn't exist
+    cp "$SOURCE_FILE" "$DESTINATION_PATH/projectGenerator.exe"
+    echo "projectGenerator.exe File copied successfully."
+else
+    # File does not exist
+    echo " projectGenerator.exe Error: Source file does not exist."
+fi
