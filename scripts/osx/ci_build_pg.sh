63975db3a4987cf8110b615f1cfd43f11de815e8
Dan Rosser <danoli3@gmail.com>
2023-12-19 06:21:43 +1100

    Split Build Steps for PG into seperate files. macOS / VS now should be easily built  (#450)

--- scripts/osx/ci_build_pg.sh
+++ scripts/osx/ci_build_pg.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 set -e
 
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPT_DIR="$( cd "$( dirname "${CURRENT_DIR}"/../../ )" && pwd )"
+PG_DIR="$( cd "$( dirname "${SCRIPT_DIR}/../../" )" && pwd )"
+
+OF_DIR="$( cd "$( dirname "${PG_DIR}/../../../" )" && pwd )"
+
+FRONTEND_DIR="$( cd "$( dirname "${PG_DIR}/frontend" )" && pwd )"
+
+CMDLINE_DIR="$( cd "$( dirname "${PG_DIR}/commandLine" )" && pwd )"
+
+
+echo "CURRENT_DIR:  ${CURRENT_DIR}"
+echo "SCRIPT_DIR:  ${SCRIPT_DIR}"
+echo "PG_DIR:  ${PG_DIR}"
+echo "FRONTEND_DIR:  ${FRONTEND_DIR}"
+echo "CMD_DIR:  ${CMD_DIR}"
+echo "====== OF_DIR: ${OF_DIR}"
+
 echoDots(){
 	sleep 0.1 # Waiting for a brief period first, allowing jobs returning immediatly to finish
 	while isRunning $1; do
@@ -18,27 +36,52 @@ echoDots(){
 }
 
 package_app(){
-	if [[ "${GITHUB_REF##*/}" == "master" &&  -z "${GITHUB_HEAD_REF}" ]] ; then
+	if [[ ("${GITHUB_REF##*/}" == "master" || "${GITHUB_REF##*/}" == "bleeding") && -z "${GITHUB_HEAD_REF}" ]] ; then
 
 		PLATFORM=$1
 		# Copy commandLine into electron .app
-		cd ${pg_root}
+		cd ${PG_DIR}
+		echo "Current directory: (should be projectGenerator)"
+		pwd
+		echo "Directory contents:"
+		ls
+		echo "-------------"
+		#echo "copy cmdline PG to "
 		cp commandLine/bin/projectGenerator projectGenerator-$PLATFORM/projectGenerator.app/Contents/Resources/app/app/projectGenerator 2> /dev/null
-
+		cd ${PG_DIR}
+		pwd
+		echo "Directory contents:"
+		ls
+		echo "-------------"
 		sed -i -e "s/osx/$PLATFORM/g" projectGenerator-$PLATFORM/projectGenerator.app/Contents/Resources/app/settings.json
 
 		# Sign app
 		echo "Signing electron .app"
-		cd ${pg_root}
-		xattr -cr projectGenerator-$PLATFORM/projectGenerator.app
-
+		echo "cd to ${PG_DIR}/projectGenerator-$PLATFORM"
+		cd ${PG_DIR}/projectGenerator-$PLATFORM
+		pwd
+		echo "Directory contents:"
+		ls
+		echo "-------------"
+		xattr -cr projectGenerator.app
+		echo "-------------"
+		echo "cd to ${PG_DIR}"
+		cd ${PG_DIR}
 		electron-osx-sign projectGenerator-$PLATFORM/projectGenerator.app --platform=darwin --type=distribution --no-gatekeeper-assess --hardened-runtime --entitlements=scripts/osx/PG.entitlements --entitlements-inherit=scripts/osx/PG.entitlements
 
+		${SCRIPT_DIR}/secure.sh projectGenerator-$PLATFORM/projectGenerator.app/Contents/MacOS/projectGenerator projectGenerator-$PLATFORM
 		echo "Compressing PG app"
 		# need to upload zip of just app to apple for notarizing
 		zip --symlinks -r -q projectGenerator-$PLATFORM/projectGenerator-$PLATFORM.zip projectGenerator-$PLATFORM/projectGenerator.app
 		xcrun altool --notarize-app --primary-bundle-id "com.electron.projectgenerator" --username "${GA_APPLE_USERNAME}" -p "${GA_APPLE_PASS}" --asc-provider "${GA_NOTARIZE_PROVIDER}" --file projectGenerator-$PLATFORM/projectGenerator-$PLATFORM.zip
-
+		mv projectGenerator-$PLATFORM/projectGenerator-$PLATFORM.zip ${PG_DIR}/../../../projectGenerator/projectGenerator-$PLATFORM.zip
+		cd ${PG_DIR}/../../../projectGenerator
+		echo "Final Directory contents: ${PG_DIR}/../../../projectGenerator"
+		pwd
+		ls
+		echo "-------------"
+	else
+		echo "package_app not on master/bleeding so will not package app"	
 	fi
 }
 
@@ -46,7 +89,7 @@ package_app(){
 sign_and_upload(){
 	PLATFORM=$1
 	# Copy commandLine into electron .app
-	cd ${pg_root}
+	cd ${PG_DIR}
 	cp commandLine/bin/projectGenerator projectGenerator-$PLATFORM/projectGenerator.app/Contents/Resources/app/app/projectGenerator 2> /dev/null
 
 	sed -i -e "s/osx/$PLATFORM/g" projectGenerator-$PLATFORM/projectGenerator.app/Contents/Resources/app/settings.json
@@ -54,10 +97,11 @@ sign_and_upload(){
 	if [[ -z "${GA_CI_SECRET}" ]] ; then
 		echo " Not on main repo skipping sign and upload ";
 	else
-		if [[ "${TRAVIS_REPO_SLUG}/${TRAVIS_BRANCH}" == "openframeworks/projectGenerator/master" && "$TRAVIS_PULL_REQUEST" == "false" ]] || [[ "${GITHUB_REF##*/}" == "master" &&  -z "${GITHUB_HEAD_REF}" ]] ; then
-			# Sign app
+		if [[ ("${TRAVIS_REPO_SLUG}/${TRAVIS_BRANCH}" == "openframeworks/projectGenerator/master" || "${TRAVIS_REPO_SLUG}/${TRAVIS_BRANCH}" == "openframeworks/projectGenerator/bleeding") && "$TRAVIS_PULL_REQUEST" == "false" ]] || 
+   			[[ ("${GITHUB_REF##*/}" == "master" || "${GITHUB_REF##*/}" == "bleeding") && -z "${GITHUB_HEAD_REF}" ]] ; then
+    		# Sign app
 			echo "Signing electron .app"
-			cd ${pg_root}
+			cd ${PG_DIR}
 			xattr -cr projectGenerator-$PLATFORM/projectGenerator.app
 			# codesign --deep --force --verbose --sign "Developer ID Application: Arturo Castro" "projectGenerator-$PLATFORM/projectGenerator.app"
 
@@ -95,7 +139,7 @@ import_certificate(){
 
 	echo "import_certificate"
 
-	if [[ "${GITHUB_REF##*/}" == "master" && -z "${GITHUB_HEAD_REF}" && -n "${CERTIFICATE_OSX_APPLICATION}" ]]; then
+	if [[ ("${GITHUB_REF##*/}" == "master" || "${GITHUB_REF##*/}" == "bleeding") && -z "${GITHUB_HEAD_REF}" && -n "${CERTIFICATE_OSX_APPLICATION}" ]]; then
 		echo "Decoding signing certificates"
 
 		KEY_CHAIN=build.keychain
@@ -126,115 +170,54 @@ import_certificate(){
 
 }
 
-import_certificate_travis(){
-	echo "${TRAVIS_REPO_SLUG}/${TRAVIS_BRANCH}";
-	if [ "${TRAVIS_REPO_SLUG}/${TRAVIS_BRANCH}" = "openframeworks/projectGenerator/master" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-		echo "Decoding signing certificates"
-		cd ${pg_root}/scripts
-		openssl aes-256-cbc -K $encrypted_b485a78f2982_key -iv $encrypted_b485a78f2982_iv -in developer_ID.p12.enc -out developer_ID.p12 -d
-		echo "Creating keychain"
-		security create-keychain -p mysecretpassword build.keychain
-		security -v list-keychains -s build.keychain "$HOME/Library/Keychains/login.keychain"
-		echo "Setting keychain as default"
-		security default-keychain -s build.keychain
-		echo "Unlocking keychain"
-		security unlock-keychain -p mysecretpassword build.keychain
-		security set-keychain-settings -t 3600 -u build.keychain
-		echo "Importing signing certificates"
-		sudo security import developer_ID.p12 -k build.keychain -P $CERT_PWD -T /usr/bin/codesign
-		# security set-key-partition-list -S apple-tool:,apple: -s -k mysecretpassword build.keychain
-		# security find-identity -v
-	fi
-}
-
-cd ..
-of_root=${PWD}/openFrameworks
-pg_root=${PWD}/openFrameworks/apps/projectGenerator
-
-if [ -d "openframeworks/.git" ]; then
-	echo 'OF already cloned, using it'
-	cd openframeworks
-	git pull
-	# git submodule init
-	# git submodule update
-	# git submodule update
-	cd ..
-	# Control will enter here if $DIRECTORY exists.
-else
-	git clone --depth=1 https://github.com/openframeworks/openFrameworks
-fi
-#git clone --depth=1 https://github.com/openframeworks/openFrameworks
-#cp not move so github actions can do cleanup without error
-cp -r projectGenerator openFrameworks/apps/
-
-cd ${of_root}
-if [ -d "libs/glfw" ]; then
-	echo 'libs installed, using them'
-else
-	scripts/osx/download_libs.sh
-fi
-
-
+echo "build_cmdline.sh"
 # Compile commandline tool
-cd ${pg_root}
-echo "Building openFrameworks PG - OSX"
-xcodebuild -configuration Release -target commandLine CODE_SIGN_IDENTITY="" UseModernBuildSystem=NO -project commandLine/commandLine.xcodeproj
-ret=$?
-if [ $ret -ne 0 ]; then
-	  echo "Failed building Project Generator"
-	  exit 1
-fi
+${CURRENT_DIR}/build_cmdline.sh
 
-cd commandLine/bin/
+echo "test_cmdline.sh"
+# Test commandline tool
+${CURRENT_DIR}/test_cmdline.sh
 
-echo "Testing project generation osx";
-chmod +x projectGenerator
-./projectGenerator --recursive -posx -o../../../../ ../../../../examples/
-errorcode=$?
-if [[ $errorcode -ne 0 ]]; then
-		exit $errorcode
-fi
 
-# install electron sign globally
-# sudo npx create-react-app electron-osx-sign
-sudo npm install -g electron-osx-sign
-
-if [ -d "/Users/runner/" ]; then
-	sudo chown -R 501:20 "/Users/runner/.npm"
-fi
-
+echo "import_certificate"
 import_certificate
-
 # Generate electron app
-cd ${pg_root}/frontend
-npm update
-npm install > /dev/null
 
-npm run build:macos > /dev/null
-if [ -d "${pg_root}/projectGenerator-osx" ]; then
-	rm -rf ${pg_root}/projectGenerator-osx
+echo "build_frontend"
+${CURRENT_DIR}/build_frontend.sh
+
+echo "Current directory: (should be projectGenerator)"
+pwd
+echo "Directory contents:"
+ls
+
+if [ -d "${PG_DIR}/projectGenerator-osx" ]; then
+	rm -rf ${PG_DIR}/projectGenerator-osx
 fi
-mv dist/projectGenerator-darwin-universal ${pg_root}/projectGenerator-osx
+
+cd ${PG_DIR}
+echo "Current directory: (should be apps/projectGenerator)"
+pwd
+echo "Directory contents:"
+ls
+echo "-------------"
+mv frontend/dist/mac-universal ${PG_DIR}/projectGenerator-osx
+
+echo "-------------"
+echo "package_app osx"
 package_app osx
 
-cd ${pg_root}/frontend
-npm run build:macos > /dev/null
-if [ -d "${pg_root}/projectGenerator-ios" ]; then
-	rm -rf ${pg_root}/projectGenerator-ios
-fi
-mv dist/projectGenerator-darwin-universal ${pg_root}/projectGenerator-ios
-package_app ios
+# echo "package_app ios"
+# npm run build:macos > /dev/null
 
-#cd ${pg_root}/frontend
-#npm run build:osx > /dev/null
-#mv dist/projectGenerator-darwin-universal ${pg_root}/projectGenerator-android
-#package_app android
+# if [ -d "${PG_DIR}/projectGenerator-ios" ]; then
+# 	rm -rf ${PG_DIR}/projectGenerator-ios
+# fi
+# mv dist/mac ${PG_DIR}/projectGenerator-ios
+# echo "package_app ios"
+# package_app ios
 
+echo "for security remove keys maybe imported"
 rm -rf scripts/id_rsa 2> /dev/null
 rm -rf scripts/*.p12 2> /dev/null
 
-
-# pwd
-# ls -alfR
-# cd ..
-# pwd
