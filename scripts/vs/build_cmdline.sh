10b53498165cd585c1874975a37c8e83dbc3bcd7
Dan Rosser <danoli3@gmail.com>
2024-02-04 14:41:28 +1100

    Fix MSBuild paths in some bash - exclude .mm/storyboard vs

--- scripts/vs/build_cmdline.sh
+++ scripts/vs/build_cmdline.sh
@@ -20,6 +20,7 @@ cd commandLine
 
 echo "Building openFrameworks commandLine PG - VS"
 
+
 if [ -z "${PLATFORM+x}" ]; then
     PLATFORM=x64
 fi
@@ -27,8 +28,17 @@ fi
 if [ -z "${TOOLSET+x}" ]; then
     TOOLSET=v143
 fi
+
+if [ -z "${CONFIGURATION+x}" ]; then
+    CONFIGURATION=${CONFIGURATION:-Release}
+fi
+
+
+
 pwd
 ls
+
+
 
 CURRENT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -36,15 +46,15 @@ CURRENT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Check if msbuild is available in PATH
 if command -v msbuild &> /dev/null; then
     echo "msbuild is available, use it directly"
-    msbuild "commandLine.vcxproj" /p:configuration=release /p:platform=${PLATFORM} /p:PlatformToolset=${TOOLSET}
+    msbuild "commandLine.vcxproj" //p:configuration=${CONFIGURATION} //p:platform=${PLATFORM} //p:PlatformToolset=${TOOLSET}
 else
     echo "msbuild is not available, use the specified path"
     #VS_BASE_PATH="/c/Program Files/Microsoft Visual Studio/2022/Community"
     #MSBUILD_PATH="${VS_BASE_PATH}/MSBuild/Current/Bin/MSBuild.exe"
 
-    MSBUILD_PATH="C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe"
-    MSSTRING=commandLine.sln /p:configuration=release /p:platform=${PLATFORM} /p:PlatformToolset=${TOOLSET}
-	"${MSBUILD_PATH}" "${MSSTRING}"
+    MSBUILD_PATH="C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\MSBuild\\Current\\Bin\\MSBuild.exe"
+    MSSTRING="//p:configuration=${CONFIGURATION} //p:platform=${PLATFORM} //p:PlatformToolset=${TOOLSET}"
+    "${MSBUILD_PATH}" commandLine.sln ${MSSTRING}
 
 fi
 
