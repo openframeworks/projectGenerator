93d8c38f6d2de9456f317823c8a210567110dd5b
Dan Rosser <danoli3@gmail.com>
2024-08-21 22:18:50 +1000

    bash - fix vs test script when generator already converted to final

--- scripts/vs/test_cmdline.sh
+++ scripts/vs/test_cmdline.sh
@@ -5,21 +5,33 @@ SCRIPT_DIR="$( cd "$( dirname "${CURRENT_DIR}"/../../ )" && pwd )"
 PG_DIR="$( cd "$( dirname "${SCRIPT_DIR}"/../../ )" && pwd )"
 
 CMD_DIR="${PG_DIR}/commandLine"
-EXE_FILE="commandLine.exe"  # Update this variable with the name of your executable
+EXE_FILE="projectGenerator.exe"
 
 echo "CURRENT_DIR:  ${CURRENT_DIR}"
 echo "SCRIPT_DIR:  ${SCRIPT_DIR}"
 echo "PG_DIR:  ${PG_DIR}"
 echo "CMD_DIR:  ${CMD_DIR}"
-echo "Executable File: ${EXE_FILE}"
-echo "====== ${CMD_DIR}"
 
-# Compile commandline tool
+echo "====== ${CMD_DIR}"
 
 cd "${CMD_DIR}/bin"
 pwd
 ls
+if [[ -f "${EXE_FILE}" ]]; then
+    echo "File ${EXE_FILE} exists."
+else
+    echo "File ${EXE_FILE} does not exist. trying commandLine.exe"
+    EXE_FILE="commandLine.exe"
+    if [[ -f "${EXE_FILE}" ]]; then
+        echo "File ${EXE_FILE} exists."
+    else
+        echo "No commandLine.exe either! Fail! exiting"
+        exit 1
+    fi
+fi
+echo "Executable File: ${EXE_FILE}"
 echo "Testing projectGenerator: [vs]"
+
 
 chmod +x "${EXE_FILE}"
 
