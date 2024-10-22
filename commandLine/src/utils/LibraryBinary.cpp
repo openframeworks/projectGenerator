ac763b503b451c00b650d714b22ff9f0717cd3c1
Roy Macdonald <macdonald.roy@gmail.com>
2024-10-22 18:10:31 -0300

    added missing include

--- commandLine/src/utils/LibraryBinary.cpp
+++ commandLine/src/utils/LibraryBinary.cpp
@@ -1,4 +1,5 @@
 #include "LibraryBinary.h"
+#include "ofFileUtils.h"
 
 const std::vector<std::string> LibraryBinary::archs { "x86", "Win32", "x64", "armv7", "ARM64", "ARM64EC" };
 const std::vector<std::string> LibraryBinary::targets{ "Debug", "Release" };
