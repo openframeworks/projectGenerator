74afcd48555ba9b3d0c430b2853ddad306a89b63
Dimitre <dimitre@users.noreply.github.com>
2025-03-28 19:33:41 -0300

    [macOS] fix for projects inside OF folder but in arbitrary depth (#633)

--- commandLine/src/projects/xcodeProject.cpp
+++ commandLine/src/projects/xcodeProject.cpp
@@ -220,14 +220,25 @@ bool xcodeProject::loadProjectFile() { //base
 		addCommand("Set :objects:E4B69B5B0A3A1756003C02F2:path string " + projectName + "Debug.app");
 	}
 	
-	// if ofRoot is not relative to the project path,
-	// set correct addons and openFrameworks folders
+	// Next block updates ofPath in xcode addons and openframeworks folder
+	// only if it is not the usual ../../.. path
+	
+	bool updateOFPath = false;
+	fs::path of = getOFRoot();
 	if (!ofIsPathInPath(fs::current_path(), getOFRoot())) {
-		addCommand("Set :objects:" + folderUUID["openFrameworks"] + ":path string " + getOFRoot().string() + "/libs/openFrameworks");
+		updateOFPath = true;
 		addCommand("Set :objects:" + folderUUID["openFrameworks"] + ":sourceTree string <absolute>");
-		
-		addCommand("Set :objects:" + folderUUID["addons"] + ":path string " + getOFRoot().string() + "/addons");
 		addCommand("Set :objects:" + folderUUID["addons"] + ":sourceTree string <absolute>");
+	} else {
+		// inside OF Root but not in usual depth
+		if (!fs::equivalent(getOFRoot(), "../../..")) {
+			updateOFPath = true;
+			of = fs::relative(getOFRoot(), fs::current_path());
+		}
+	}
+	if (updateOFPath) {
+		addCommand("Set :objects:" + folderUUID["openFrameworks"] + ":path string " + of.string() + "/libs/openFrameworks");
+		addCommand("Set :objects:" + folderUUID["addons"] + ":path string " + of.string() + "/addons");
 	}
 
 	return true;
