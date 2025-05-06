5b62dabcf28605d11018a6c94d36e1986fb1e577
tarnith <tarnith@gmail.com>
2025-05-06 15:41:24 -0600

    Include algorithm for std::find call (#636)

--- commandLine/src/utils/LibraryBinary.cpp
+++ commandLine/src/utils/LibraryBinary.cpp
@@ -1,5 +1,6 @@
 #include "LibraryBinary.h"
 #include "ofFileUtils.h"
+#include <algorithm>
 
 const std::vector<std::string> LibraryBinary::archs { "x86", "Win32", "x64", "armv7", "ARM64", "ARM64EC" };
 const std::vector<std::string> LibraryBinary::targets{ "Debug", "Release" };
