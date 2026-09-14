57f23fc1dd56eaafe43c81773e7d976d22b85290
Dan Rosser <danoli3@gmail.com>
2026-09-14 16:45:25 +1000

    Quote remaining keychain/cert vars in import_certificate

--- scripts/osx/ci_build_pg.sh
+++ scripts/osx/ci_build_pg.sh
@@ -219,24 +219,24 @@ import_certificate(){
         CERTIFICATE_P12=certificate.p12
 
         # Recreate the certificate from the secure environment variable
-        echo $CERTIFICATE_OSX_APPLICATION | base64 --decode > $CERTIFICATE_P12
+        echo "$CERTIFICATE_OSX_APPLICATION" | base64 --decode > "$CERTIFICATE_P12"
 
         #create a keychain
-        security create-keychain -p actions $KEY_CHAIN
+        security create-keychain -p actions "$KEY_CHAIN"
 
         # Make the keychain the default so identities are found
-        security default-keychain -s $KEY_CHAIN
+        security default-keychain -s "$KEY_CHAIN"
 
         # Unlock the keychain
-        security unlock-keychain -p actions $KEY_CHAIN
+        security unlock-keychain -p actions "$KEY_CHAIN"
 
         echo "Importing signing certificates"
-        security import $CERTIFICATE_P12 -k $KEY_CHAIN -P "$CERTIFICATE_PASSWORD" -T /usr/bin/codesign;
+        security import "$CERTIFICATE_P12" -k "$KEY_CHAIN" -P "$CERTIFICATE_PASSWORD" -T /usr/bin/codesign;
 
-        security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k actions $KEY_CHAIN
+        security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k actions "$KEY_CHAIN"
 
         # makes keychain not timeout
-        security set-keychain-settings $KEY_CHAIN
+        security set-keychain-settings "$KEY_CHAIN"
 
         echo "import certificates done"
     fi
